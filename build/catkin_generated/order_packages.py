8e0ec13cff89fb1c8eed15af9a439e99b0c28fd0
Your Name <you@example.com>
2025-01-09 14:08:50 +0530

    Add existing folder to the repository

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/tsk/terrain_mapping/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/tsk/terrain_mapping/devel;/home/tsk/suas_ws/devel;/home/tsk/ros_ws/devel;/opt/ros/noetic'.split(';') if '/home/tsk/terrain_mapping/devel;/home/tsk/suas_ws/devel;/home/tsk/ros_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/isdc_drone/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tsk/terrain_mapping/src/isdc_drone
+# Build directory: /home/tsk/terrain_mapping/build/isdc_drone
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
